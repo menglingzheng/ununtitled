417d95eea88f1049d2538cd4d9c034a03de8f0d6
mlzheng <2455156355@qq.com>
2021-08-19 20:55:43 +0800

    second commit

--- src/main/java/com/mlzheng/Mlzheng.java
+++ src/main/java/com/mlzheng/Mlzheng.java
@@ -3,5 +3,7 @@ package com.mlzheng;
 public class Mlzheng {
     public static void main(String[] args) {
         System.out.println("nihao");
+        System.out.println("nihao");
+        System.out.println("nihao");
     }
 }
